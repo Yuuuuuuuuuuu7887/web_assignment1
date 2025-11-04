d7172035c1d62a9492917d55f03b4c58c6b01e9e
“Yuuuuuuuuuuu7887“ <yutika0342@gmail.com>
2025-11-04 23:40:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -232,5 +232,5 @@ app.post("/logs", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
